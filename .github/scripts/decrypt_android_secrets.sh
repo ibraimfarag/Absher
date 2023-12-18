c6b809b626629e344cf0ec027389de35d0d6eee0
ibrahim ahmed <84781424+ibraimfarag@users.noreply.github.com>
2023-12-18 19:30:51 +0200

    Update decrypt_android_secrets.sh

--- .github/scripts/decrypt_android_secrets.sh
+++ .github/scripts/decrypt_android_secrets.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="${{secrets.ANDROID_KEYS_ZIP_PASSPHRASE}}" \
+gpg --quiet --batch --yes --decrypt --passphrase="$secrets.ANDROID_KEYS_ZIP_PASSPHRASE" \
 --output android/files.zip android/files.zip.gpg && cd android && jar xvf files.zip && cd -
 ls -d $PWD/android/*
 # mv ./android/expensemanager.jks ./android/app
